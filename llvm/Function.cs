3928e505ed7e9cfcf93b36f4f6fe9af181569705
Christophe Augier <christophe.augier@gmail.com>
2011-10-10 22:08:00 +0200

    Fixed GetReturnType()

--- llvm/Function.cs
+++ llvm/Function.cs
@@ -5,17 +5,20 @@ namespace LLVM {
 
 public class Function : Value { // should inherit GlobalValue -> Constant -> User -> Value
 
+    FunctionType _type;
+
     public Function(Module mod, string name, FunctionType fnTy) : base(LLVM.AddFunction(mod, name, fnTy))
     {
+        _type = fnTy;
     }
 
     public Type GetReturnType()
     {
-	IntPtr ptr = LLVM.GetReturnType(this);
-	if (ptr == IntPtr.Zero) {
-	    return Type.GetVoid();
-	}
-	return Type.GetType(ptr);
+        IntPtr ptr = LLVM.GetReturnType(this._type);
+        if (ptr == IntPtr.Zero) {
+            return Type.GetVoid();
+        }
+        return Type.GetType(ptr);
     }
 
     public uint GetNumParams()
