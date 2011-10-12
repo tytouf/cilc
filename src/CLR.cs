274a8d5c7fa395f9645a7a4917656da0bc3d139e
Christophe Augier <christophe.augier@gmail.com>
2011-10-12 23:05:22 +0200

    Add Simplified BSD License

--- src/CLR.cs
+++ src/CLR.cs
@@ -1,4 +1,9 @@
 
+// Copyright 2011, Christophe Augier. All rights reserved.
+//
+// This file is distributed under the Simplified BSD License. See LICENSE.TXT
+// for details.
+
 using System;
 using LLVM;
 
--- src/CodeGenMethod.cs
+++ src/CodeGenMethod.cs
@@ -1,4 +1,8 @@
 
+// Copyright 2011, Christophe Augier. All rights reserved.
+//
+// This file is distributed under the Simplified BSD License. See LICENSE.TXT
+// for details.
 
 using System;
 using System.Linq;
